bb2cfc3b26cbd185ef71c876f6d392d3df47895d
Saltuk Kezer <saltukkezer@gmail.com>
2022-04-07 13:22:14 +0200

    Webpack now bundling out app code

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,19 @@
-const alertService = new AlertService();
-const componentService = new ComponentService();
+import { inputsAreValid } from './utils/inputs-are-valid';
+import { parseInputs } from './utils/parse-inputs';
 
-const run = (alertService, componentService) => {
-  alertService.hideErrors();
+export const run = (alertService, componentService) => {
+	alertService.hideErrors();
 
-  componentService.onClick(() => {
-    alertService.hideErrors();
-    const inputs = componentService.getInputs();
-    const parsedInputs = parseInputs(...inputs);
-    if(inputsAreValid(...parsedInputs)){
-      const [numA, numB] = parsedInputs;
-      componentService.setResult(numA + numB);
-    } else {
-      componentService.setResult("");
-      alertService.handleAdditionError(inputs, parsedInputs);
-    }
-  });
+	componentService.onClick(() => {
+		alertService.hideErrors();
+		const inputs = componentService.getInputs();
+		const parsedInputs = parseInputs(...inputs);
+		if (inputsAreValid(...parsedInputs)) {
+			const [ numA, numB ] = parsedInputs;
+			componentService.setResult(numA + numB);
+		} else {
+			componentService.setResult('');
+			alertService.handleAdditionError(inputs, parsedInputs);
+		}
+	});
 };
-run(alertService, componentService);
