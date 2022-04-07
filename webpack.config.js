24317921daff54f28c8c578ce2f922713b9be515
Saltuk Kezer <saltukkezer@gmail.com>
2022-04-07 14:19:25 +0200

    Add first loaders to handle CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
 
 module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"] //It needs to be in inversed order. The loaders from the last of the list are first loaded and applied
+            },
+        ],
+    },
     mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     },
-
 };
