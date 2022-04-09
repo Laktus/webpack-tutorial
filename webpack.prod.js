57e5c15892d06d0500bd933b2551b313b639670d
Saltuk Kezer <saltukkezer@gmail.com>
2022-04-09 12:57:02 +0200

    Minify HTML, CSS, and JS in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,27 @@
-const path = require("path");
-const common = require("./webpack.common")
-const { merge } = require("webpack-merge")
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const path = require('path');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
-    mode: "production",
-    output: {
-        filename: "[name].[contenthash].bundle.js", //Cache Busting
-        path: path.resolve(__dirname, "dist"),
-        assetModuleFilename: 'imgs/[name].[hash].[ext]'
-    },
-    plugins: [new CleanWebpackPlugin()]
+	mode: 'production',
+	output: {
+		filename: '[name].[contenthash].bundle.js', //Cache Busting
+		path: path.resolve(__dirname, 'dist'),
+		assetModuleFilename: 'imgs/[name].[hash].[ext]'
+	},
+	plugins: [ new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }), new CleanWebpackPlugin() ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Extract css into files
+                    "css-loader", //2. Turns css into commonjs
+                    "sass-loader" //1. Turns sass into css
+                ] 
+            },
+        ]
+    }
 });
